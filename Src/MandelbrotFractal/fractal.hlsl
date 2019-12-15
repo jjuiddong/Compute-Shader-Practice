5e6920187b57d8b23522d764bef8ce17e23d90e7
jjuiddong <jjuiddong@hanmail.net>
2019-12-16 00:52:20 +0900

    update mandelbrot fractal compute shader project

---
+++ Src/MandelbrotFractal/fractal.hlsl
@@ -0,0 +1,67 @@
+//--------------------------------------------------------------------------------------
+// File: fractal.hlsl
+//
+// This is a compute shader that draws a Mandelbrot fractal.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+Texture2D ColorMapTexture : register(t0);
+RWTexture2D<float4> OutputTexture : register(u0);
+
+
+cbuffer cb0 : register(b6)
+{
+	float4 g_MaxThreadIter;
+	float4 g_Window;
+}
+
+SamplerState ColorMapSampler : register(s0)
+{
+	Filter = MIN_MAG_LINEAR_MIP_POINT;
+	AddressU = Clamp;
+	AddressV = Clamp;
+	AddressW = Clamp;
+	BorderColor = float4(1, 1, 1, 1);
+};
+
+
+[numthreads(8, 8, 1)]
+void main(uint3 DTid : SV_DispatchThreadID)
+{
+	float2 WindowLocal = ((float2)DTid.xy / g_MaxThreadIter.xy) * float2(1, -1) + float2(-0.5f, 0.5f);
+	float2 coord = WindowLocal.xy * g_Window.xy + g_Window.zw;
+
+	uint maxiter = (uint)g_MaxThreadIter.z * 4;
+	uint iter = 0;
+	float2 constant = coord;
+	float2 sq;
+	do
+	{
+		float2 newvalue;
+		sq = coord * coord;
+		newvalue.x = sq.x - sq.y;
+		newvalue.y = 2 * coord.y * coord.x;
+		coord = newvalue + constant;
+		iter++;
+	} while (iter < maxiter && (sq.x + sq.y) < 4.0);
+
+	float colorIndex = frac((float)iter / g_MaxThreadIter.z);
+	float4 SampledColor = ColorMapTexture.SampleLevel(ColorMapSampler, float2(colorIndex, 0), 0);
+
+	OutputTexture[DTid.xy] = SampledColor;
+}
+
+
+technique11 Compute
+{
+	pass P0
+	{
+		SetVertexShader(NULL);
+		SetComputeShader(CompileShader(cs_5_0, main()));
+		SetGeometryShader(NULL);
+		SetHullShader(NULL);
+		SetDomainShader(NULL);
+		SetPixelShader(NULL);
+	}
+}
---
+++ Src/MandelbrotFractal/mandelbrotfractal.cpp
@@ -0,0 +1,168 @@
+//
+// Mandelbrot Fractal using Compute Shader
+//
+// reference
+//		https://docs.microsoft.com/ko-kr/samples/microsoft/xbox-atg-samples/simplecomputepc/
+//
+#include "../../../Common/Common/common.h"
+#include "../../../Common/Graphic11/graphic11.h"
+#include "../../../Common/Framework11/framework11.h"
+using common::StrId;
+using common::StrPath;
+using common::Vector4;
+using common::Vector3;
+using common::Vector2;
+using common::sRectf;
+
+using namespace graphic;
+using namespace framework;
+
+struct sCbFractal
+{
+	XMVECTOR MaxThreadIter;
+	XMVECTOR Window;
+};
+
+class cViewer : public framework::cGameMain2
+{
+public:
+	cViewer();
+	virtual ~cViewer();
+	virtual bool OnInit() override;
+	virtual void OnUpdate(const float deltaSeconds) override;
+	virtual void OnRender(const float deltaSeconds) override;
+	virtual void OnEventProc(const sf::Event &evt) override;
+
+public:
+	graphic::cComputeShader m_computeShader;
+	graphic::cTexture m_fractalTexture;
+	graphic::cConstantBuffer<sCbFractal> m_cbFractal;
+	graphic::cTexture m_fractalColorMap;
+	graphic::cQuad2D m_fractalQuad;
+};
+
+INIT_FRAMEWORK3(cViewer);
+
+struct BufType
+{
+	int i;
+	float f;
+
+	void SetValue(int i, float f)
+	{
+		this->i = i;
+		this->f = f;
+	}
+};
+
+::std::vector<BufType> g_vBuf0, g_vBuf1;
+const int NUM_ELEMENTS = 100;
+
+
+cViewer::cViewer()
+{
+	graphic::cResourceManager::Get()->SetMediaDirectory("./media/");
+
+	m_windowName = L"Simple Compute Shader";
+	m_isLazyMode = true;
+	const RECT r = { 0, 0, 1024, 768 };
+	m_windowRect = r;
+}
+
+cViewer::~cViewer()
+{
+}
+
+
+bool cViewer::OnInit()
+{
+	const float WINSIZE_X = float(m_windowRect.right - m_windowRect.left);
+	const float WINSIZE_Y = float(m_windowRect.bottom - m_windowRect.top);
+	GetMainCamera().SetCamera(Vector3(30, 30, -30), Vector3(0, 0, 0), Vector3(0, 1, 0));
+	GetMainCamera().SetProjection(MATH_PI / 4.f, (float)WINSIZE_X / (float)WINSIZE_Y, 0.1f, 10000.0f);
+	GetMainCamera().SetViewPort(WINSIZE_X, WINSIZE_Y);
+
+	m_camera.SetCamera(Vector3(-3, 10, -10), Vector3(0, 0, 0), Vector3(0, 1, 0));
+	m_camera.SetProjection(MATH_PI / 4.f, (float)WINSIZE_X / (float)WINSIZE_Y, 1.0f, 10000.f);
+	m_camera.SetViewPort(WINSIZE_X, WINSIZE_Y);
+
+	GetMainLight().Init(cLight::LIGHT_DIRECTIONAL,
+		Vector4(0.3f, 0.3f, 0.3f, 1), Vector4(0.9f, 0.9f, 0.9f, 1),
+		Vector4(0.2f, 0.2f, 0.2f, 1));
+	const Vector3 lightPos(-300, 300, -300);
+	const Vector3 lightLookat(0, 0, 0);
+	GetMainLight().SetPosition(lightPos);
+	GetMainLight().SetDirection((lightLookat - lightPos).Normal());
+	m_gui.SetContext();
+	m_gui.SetContext();
+	m_gui.SetStyleColorsDark();
+
+	cRenderer &renderer = m_renderer;
+	sRectf rect = m_windowRect;
+
+	bool result = false;
+	result = m_computeShader.Create(renderer, "./fractal.cso");
+	result = m_fractalTexture.Create(renderer, (int)rect.Width(), (int)rect.Height()
+		, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_USAGE_DEFAULT, true);
+	result = m_cbFractal.Create(renderer);
+
+	const uint GradientTexels[8] = { 0xFF000040, 0xFF000080, 0xFF0000C0, 0xFF0000FF, 0xFF0040FF, 0xFF0080FF, 0xFF00C0FF, 0xFF00FFFF };
+	result = m_fractalColorMap.Create(renderer, 8, 1
+		, DXGI_FORMAT_R8G8B8A8_UNORM, GradientTexels, sizeof(GradientTexels)
+		, D3D11_USAGE_DEFAULT, false);
+
+	cTexture *tex = cResourceManager::Get()->LoadTexture(renderer, "box.dds");
+	m_fractalQuad.Create(renderer, 0, 0, rect.Width(), rect.Height());
+	m_fractalQuad.m_texture = &m_fractalTexture;
+
+	return true;
+}
+
+
+void cViewer::OnUpdate(const float deltaSeconds)
+{
+	__super::OnUpdate(deltaSeconds);
+	cAutoCam cam(&m_camera);
+	GetMainCamera().Update(deltaSeconds);
+}
+
+
+void cViewer::OnRender(const float deltaSeconds)
+{
+	cRenderer &renderer = m_renderer;
+
+	m_cbFractal.m_v->MaxThreadIter =
+		Vector4((float)m_fractalTexture.m_imageInfo.Width
+			, (float)m_fractalTexture.m_imageInfo.Height
+			, 300
+			, 0).GetVectorXM();
+	m_cbFractal.m_v->Window = Vector4(4.0f, 2.25f, -0.65f, 0.0f).GetVectorXM();
+	m_cbFractal.Update(renderer, 6);
+	m_fractalColorMap.Bind(renderer, 0);
+	m_computeShader.Bind(renderer);
+	m_fractalTexture.BindUnorderedAccessView(renderer, 0);
+
+	// make sure to update value in shader if this changes
+	const uint s_numShaderThreads = 8;
+	const uint threadGroupX = m_fractalTexture.m_imageInfo.Width / s_numShaderThreads;
+	const uint threadGroupY = m_fractalTexture.m_imageInfo.Height / s_numShaderThreads;
+	renderer.GetDevContext()->Dispatch(threadGroupX, threadGroupY, 1);
+	m_fractalTexture.Unbind(renderer, 0);
+
+	m_fractalQuad.Render(renderer);
+}
+
+
+void cViewer::OnEventProc(const sf::Event &evt)
+{
+	ImGuiIO& io = ImGui::GetIO();
+	switch (evt.type)
+	{
+	case sf::Event::KeyPressed:
+		switch (evt.key.cmd)
+		{
+		case sf::Keyboard::Escape: close(); break;
+		}
+		break;
+	}
+}
